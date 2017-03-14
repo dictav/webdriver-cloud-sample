a3252a1ea4c29ae91d62f6cf7abf8ad4455e1713
Shintaro Abe <dictav@gmail.com>
2017-03-14 17:23:29 +0900

    use env

--- test/console_log.js
+++ test/console_log.js
@@ -7,6 +7,10 @@ const mockServer = require('./mock_server.js').mockServer
 
 const root = 'test/public'
 
+const domain1 = process.env.DOMAIN1 || 'localhost:3000'
+const domain2 = process.env.DOMAIN2 || 'localhost:3001'
+const domainApi = process.env.DOMAIN_API || 'localhost:3030'
+
 describe('Console Log', () => {
   before(() => {
     httpServer.createServer({ root }).listen(3000)
@@ -15,21 +19,21 @@ describe('Console Log', () => {
   })
 
   it('gets the title of page1', () => {
-    const title = browser.url('http://localhost:3000/page1.html').getTitle()
+    const title = browser.url('http://' + domain1 + '/page1.html').getTitle()
     assert.equal(title, 'DOMAIN1')
   })
 
   it('gets the title of page2', () => {
-    const title = browser.url('http://localhost:3001/page2.html').getTitle()
+    const title = browser.url('http://' + domain2 + '/page2.html').getTitle()
     assert.equal(title, 'DOMAIN2')
   })
 
   it('send beacon', () => {
-    browser.url('http://localhost:3000/beacon.html?localhost:3030')
+    browser.url('http://' + domain1 + '/beacon.html?' + domainApi)
 
     browser.call(() => {
       return new Promise((resolve, reject) => {
-        request.get({url:'http://localhost:3030/last_request', timeout: 1000}, (err, res) => {
+        request.get({url:'http://' + domainApi + '/last_request', timeout: 1000}, (err, res) => {
           const data = JSON.parse(res.body)
           assert.equal(data.method, 'GET')
           resolve()
